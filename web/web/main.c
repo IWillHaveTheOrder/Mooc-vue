a8662ea8792e38b9797756fedfdbd000c33a137f
veture <s990826972@163.com>
2021-11-18 11:04:49 +0800

    First.Commit

---
+++ web/web/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  web
+//
+//  Created by MacBook on 2021/11/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
